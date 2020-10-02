cae7feef5b4ca7a233fc4a47e60b88b0d0c2b320
Dan Boguslavsky <61335889+danbogu@users.noreply.github.com>
2020-10-02 17:23:38 +0300

    Table creation (SQL Server)

---
+++ Table creation.sql
@@ -0,0 +1,19 @@
+drop table test.dbo.CARS
+CREATE TABLE test.[dbo].[CARS](
+	[main_title] [varchar](50) NULL,
+	[second_title] [varchar](50) NULL,
+	[price] [varchar](15) NULL,
+	[year] [varchar](10) NULL,
+	[hand] [varchar](10) NULL,
+	[engineSize] [varchar](10) NULL,
+	[summary] [varchar](500) NULL,
+	[kilometers] [varchar](25) NULL,
+	[engineType] [varchar](25) NULL,
+	[gearBox] [varchar](25) NULL,
+	[color] [varchar](20) NULL,
+	[onRoadFrom] [varchar](15) NULL,
+	[testDate] [varchar](15) NULL,
+	[ownerID] [varchar](10) NULL,
+	[tradeIn] [varchar](10) NULL,
+	[disabledFriendly] [varchar](10) NULL
+) 
